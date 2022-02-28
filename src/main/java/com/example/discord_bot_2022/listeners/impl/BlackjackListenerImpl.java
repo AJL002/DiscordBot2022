40767e66940071107c7806f454ce4d2dc3973a31
Alexander Littlejohn <littlejohn2022@hotmail.com>
2022-02-28 16:58:27 -0500

    Changed clubs and spades to orange squares and circles

--- src/main/java/com/example/discord_bot_2022/listeners/impl/BlackjackListenerImpl.java
+++ src/main/java/com/example/discord_bot_2022/listeners/impl/BlackjackListenerImpl.java
@@ -24,24 +24,24 @@ public class BlackjackListenerImpl implements BlackjackListener {
             deck.put(":seven::heart:", "7");
             deck.put(":eight::heart:", "8");
             deck.put(":nine::heart:", "9");
-            deck.put(":ten::heart:", "10");
-            deck.put("JackOhReally:895893197224546304:heart:", "10");
+            deck.put(":keycap_ten::heart:", "10");
+            deck.put("<:JackOhReally:895893197224546304>:heart:", "10");
             deck.put(":woman::heart:", "10");
             deck.put(":crown::heart:", "10");
-            //CLUBS
-            deck.put(":a::clubs:", "1");
-            deck.put(":two::clubs:", "2");
-            deck.put(":three::clubs:", "3");
-            deck.put(":four::clubs:", "4");
-            deck.put(":five::clubs:", "5");
-            deck.put(":six::clubs:", "6");
-            deck.put(":seven::clubs:", "7");
-            deck.put(":eight::clubs:", "8");
-            deck.put(":nine::clubs:", "9");
-            deck.put(":ten::clubs:", "10");
-            deck.put("JackOhReally:895893197224546304:clubs:", "10");
-            deck.put(":woman::clubs:", "10");
-            deck.put(":crown::clubs:", "10");
+            //SQUARES AKA CLUBS
+            deck.put(":a::orange_square:", "1");
+            deck.put(":two::orange_square:", "2");
+            deck.put(":three::orange_square:", "3");
+            deck.put(":four::orange_square:", "4");
+            deck.put(":five::orange_square:", "5");
+            deck.put(":six::orange_square:", "6");
+            deck.put(":seven::orange_square:", "7");
+            deck.put(":eight::orange_square:", "8");
+            deck.put(":nine::orange_square:", "9");
+            deck.put(":keycap_ten::orange_square:", "10");
+            deck.put("<:JackOhReally:895893197224546304>:orange_square:", "10");
+            deck.put(":woman::orange_square:", "10");
+            deck.put(":crown::orange_square:", "10");
             //DIAMONDS
             deck.put(":a::diamonds:", "1");
             deck.put(":two::diamonds:", "2");
@@ -52,30 +52,30 @@ public class BlackjackListenerImpl implements BlackjackListener {
             deck.put(":seven::diamonds:", "7");
             deck.put(":eight::diamonds:", "8");
             deck.put(":nine::diamonds:", "9");
-            deck.put(":ten::diamonds:", "10");
-            deck.put("JackOhReally:895893197224546304:diamonds:", "10");
+            deck.put(":keycap_ten::diamonds:", "10");
+            deck.put("<:JackOhReally:895893197224546304>:diamonds:", "10");
             deck.put(":woman::diamonds:", "10");
             deck.put(":crown::diamonds:", "10");
-            //SPADES
-            deck.put(":a::spades:", "1");
-            deck.put(":two::spades:", "2");
-            deck.put(":three::spades:", "3");
-            deck.put(":four::spades:", "4");
-            deck.put(":five::spades:", "5");
-            deck.put(":six::spades:", "6");
-            deck.put(":seven::spades:", "7");
-            deck.put(":eight::spades:", "8");
-            deck.put(":nine::spades:", "9");
-            deck.put(":ten::spades:", "10");
-            deck.put("JackOhReally:895893197224546304:spades:", "10");
-            deck.put(":woman::spades:", "10");
-            deck.put(":crown::spades:", "10");
+            //CIRCLES AKA SPADES
+            deck.put(":a::orange_circle:", "1");
+            deck.put(":two::orange_circle:", "2");
+            deck.put(":three::orange_circle:", "3");
+            deck.put(":four::orange_circle:", "4");
+            deck.put(":five::orange_circle:", "5");
+            deck.put(":six::orange_circle:", "6");
+            deck.put(":seven::orange_circle:", "7");
+            deck.put(":eight::orange_circle:", "8");
+            deck.put(":nine::orange_circle:", "9");
+            deck.put(":keycap_ten::orange_circle:", "10");
+            deck.put("<:JackOhReally:895893197224546304>:orange_circle:", "10");
+            deck.put(":woman::orange_circle:", "10");
+            deck.put(":crown::orange_circle:", "10");
         }
 
         if (messageCreateEvent.getMessageContent().toLowerCase().contains("!blackjack") || messageCreateEvent.getMessageContent().toLowerCase().contains("!bj")) {
             Random rand = new Random();
             String[] val = {":a:", ":two:", ":three:",":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:", "JackOhReally:895893197224546304", ":woman:", ":crown:"};
-            String[] face = {":heart:", ":clubs:", ":diamonds:", ":spades:"};
+            String[] face = {":heart:", ":orange_square:", ":diamonds:", ":orange_circle:"};
             int x = rand.nextInt(val.length);
             int y = rand.nextInt(face.length);
             String card = val[x]+face[y];
