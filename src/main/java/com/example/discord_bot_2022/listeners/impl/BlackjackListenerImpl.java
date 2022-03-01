e5aba50796e4a4f62ab125efc2b8ddb79439a825
Alexander Littlejohn <littlejohn2022@hotmail.com>
2022-02-28 19:50:51 -0500

    Simple blackjack and messagebuilder testing

--- src/main/java/com/example/discord_bot_2022/listeners/impl/BlackjackListenerImpl.java
+++ src/main/java/com/example/discord_bot_2022/listeners/impl/BlackjackListenerImpl.java
@@ -1,12 +1,17 @@
 package com.example.discord_bot_2022.listeners.impl;
 
 import com.example.discord_bot_2022.listeners.BlackjackListener;
+import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.emoji.Emoji;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.Hashtable;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class BlackjackListenerImpl implements BlackjackListener {
@@ -15,77 +20,39 @@ public class BlackjackListenerImpl implements BlackjackListener {
         Hashtable<String, String> deck = new Hashtable<>();
         {   //DECK HASHTABLE
             //HEARTS
-            deck.put(":a::heart:", "1");
-            deck.put(":two::heart:", "2");
-            deck.put(":three::heart:", "3");
-            deck.put(":four::heart:", "4");
-            deck.put(":five::heart:", "5");
-            deck.put(":six::heart:", "6");
-            deck.put(":seven::heart:", "7");
-            deck.put(":eight::heart:", "8");
-            deck.put(":nine::heart:", "9");
-            deck.put(":keycap_ten::heart:", "10");
-            deck.put("<:JackOhReally:895893197224546304>:heart:", "10");
-            deck.put(":woman::heart:", "10");
-            deck.put(":crown::heart:", "10");
-            //SQUARES AKA CLUBS
-            deck.put(":a::orange_square:", "1");
-            deck.put(":two::orange_square:", "2");
-            deck.put(":three::orange_square:", "3");
-            deck.put(":four::orange_square:", "4");
-            deck.put(":five::orange_square:", "5");
-            deck.put(":six::orange_square:", "6");
-            deck.put(":seven::orange_square:", "7");
-            deck.put(":eight::orange_square:", "8");
-            deck.put(":nine::orange_square:", "9");
-            deck.put(":keycap_ten::orange_square:", "10");
-            deck.put("<:JackOhReally:895893197224546304>:orange_square:", "10");
-            deck.put(":woman::orange_square:", "10");
-            deck.put(":crown::orange_square:", "10");
-            //DIAMONDS
-            deck.put(":a::diamonds:", "1");
-            deck.put(":two::diamonds:", "2");
-            deck.put(":three::diamonds:", "3");
-            deck.put(":four::diamonds:", "4");
-            deck.put(":five::diamonds:", "5");
-            deck.put(":six::diamonds:", "6");
-            deck.put(":seven::diamonds:", "7");
-            deck.put(":eight::diamonds:", "8");
-            deck.put(":nine::diamonds:", "9");
-            deck.put(":keycap_ten::diamonds:", "10");
-            deck.put("<:JackOhReally:895893197224546304>:diamonds:", "10");
-            deck.put(":woman::diamonds:", "10");
-            deck.put(":crown::diamonds:", "10");
-            //CIRCLES AKA SPADES
-            deck.put(":a::orange_circle:", "1");
-            deck.put(":two::orange_circle:", "2");
-            deck.put(":three::orange_circle:", "3");
-            deck.put(":four::orange_circle:", "4");
-            deck.put(":five::orange_circle:", "5");
-            deck.put(":six::orange_circle:", "6");
-            deck.put(":seven::orange_circle:", "7");
-            deck.put(":eight::orange_circle:", "8");
-            deck.put(":nine::orange_circle:", "9");
-            deck.put(":keycap_ten::orange_circle:", "10");
-            deck.put("<:JackOhReally:895893197224546304>:orange_circle:", "10");
-            deck.put(":woman::orange_circle:", "10");
-            deck.put(":crown::orange_circle:", "10");
+            deck.put(":a:", "1");
+            deck.put(":two:", "2");
+            deck.put(":three:", "3");
+            deck.put(":four:", "4");
+            deck.put(":five:", "5");
+            deck.put(":six:", "6");
+            deck.put(":seven:", "7");
+            deck.put(":eight:", "8");
+            deck.put(":nine:", "9");
+            deck.put(":keycap_ten:", "10");
+            deck.put("<:JackOhReally:895893197224546304>", "10");
+            deck.put(":woman:", "10");
+            deck.put(":crown:", "10");
         }
 
         if (messageCreateEvent.getMessageContent().toLowerCase().contains("!blackjack") || messageCreateEvent.getMessageContent().toLowerCase().contains("!bj")) {
             Random rand = new Random();
             String[] val = {":a:", ":two:", ":three:",":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:", "<:JackOhReally:895893197224546304>", ":woman:", ":crown:"};
-            String[] face = {":heart:", ":orange_square:", ":diamonds:", ":orange_circle:"};
-            int x = rand.nextInt(val.length);
-            int y = rand.nextInt(face.length);
-            String card = val[x]+face[y];
+            String card = val[rand.nextInt(val.length)];
 
-            messageCreateEvent.getChannel().sendMessage(card);
-            messageCreateEvent.getChannel().sendMessage(deck.get(card));
+//            messageCreateEvent.getChannel().sendMessage(card);
+//            messageCreateEvent.getChannel().sendMessage(deck.get(card));
+
+            TextChannel channel = messageCreateEvent.getChannel();
+
+            new MessageBuilder()
+                    .setContent("Choose an option")
+                    .addComponents(
+                            ActionRow.of(Button.success("success", "Hit"),
+                                    Button.danger("danger", "Stay")
+                                    ))
+                    .send(channel);
+            
         }
-
-
-
     }
-
 }
---
+++ src/main/java/com/example/discord_bot_2022/services/MessagingService.java
@@ -0,0 +1,9 @@
+package com.example.discord_bot_2022.services;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.MessageAuthor;
+
+public interface MessagingService {
+
+    void sendMessage(MessageAuthor author, String title, String description, String footer, String thumbnail, TextChannel channel);
+}
